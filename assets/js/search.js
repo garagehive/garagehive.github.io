1e3f00a939c56081158156f54348d4bcb0f60a48
Aigars Lapiņš <aigarsl@elvabaltic.lv>
2019-03-04 14:09:03 +0200

    JS change 3

--- assets/js/search.js
+++ assets/js/search.js
@@ -38,20 +38,19 @@
       // Initalize lunr with the fields it will be searching on. I've given title
       // a boost of 10 to indicate matches on this field are more important.
       var idx = lunr(function () {
-        this.field('id');
         this.field('title', { boost: 10 });
         this.field('content');
       });
   
       for (var key in window.store) { // Add the data to lunr
+        window.alert("title: " + window.store[key].title);
         idx.add({
           'id': key,
           'title': window.store[key].title,
           'content': window.store[key].content
         });
-  
-        var results = idx.search(searchTerm); // Get lunr to perform a search
-        displaySearchResults(results, window.store); // We'll write this in the next section
       }
+      var results = idx.search(searchTerm); // Get lunr to perform a search
+      displaySearchResults(results, window.store); // We'll write this in the next section
     }
   })();
